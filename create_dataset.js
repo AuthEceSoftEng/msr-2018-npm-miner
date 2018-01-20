a7cabce5aaaaffa2f801196c7a2d41de8535e1ba
Mixalis Papamixail <papa.mich@hotmail.com>
2018-01-21 01:25:27 +0200

    Updated create_dataset

--- create_dataset.js
+++ create_dataset.js
@@ -1,48 +1,144 @@
 const downloader = require('./downloader/index');
 const properties = require('./properties');
 const dbmanager = require('./dbmanager/index');
+const lib = require('./utilities/library');
 const path = require('path');
+const jssa = require('jssa');
+const jsonfile = require('jsonfile')
+const bfj = require('bfj');
 
 DB_URL = properties.getCouchDBUrl();
 DB_name = properties.getDBName();
 
 function getRepositorySource(repository_information){
 
-  var repository_name = repository_information.latest_package_json.name + '-' + repository_information.latest_package_json.version;
+  return new Promise((resolve, reject) => {
+    var repository_name = repository_information.latest_package_json.name + '-' + repository_information.latest_package_json.version;
 
-  init_folder_structure = dbmanager.initRepositoryFolderStructure(properties.getStoringFolder(), repository_name);
-  init_folder_structure.then(value =>{
-    var tarball_url = repository_information.latest_package_json.dist.tarball;
-    var tarball_name = tarball_url.split('/')[tarball_url.split('/').length - 1];
-    var tarball_storage_path = properties.getStoringFolder() + path.sep + repository_name + path.sep + 'source_code' + path.sep + 'compressed';
-    var tarball_extract_path = properties.getStoringFolder() + path.sep + repository_name + path.sep + 'source_code' + path.sep + 'raw';
-    
-    downloader.getProjectTarball(tarball_url, tarball_storage_path + path.sep + tarball_name).then(value => {
-      downloader.extractProjectTarball(tarball_storage_path + path.sep + tarball_name, tarball_extract_path + path.sep + repository_name).then(value =>{
-        console.log('Downlad source code completed successfully');
+    init_folder_structure = dbmanager.initRepositoryFolderStructure(properties.getStoringFolder(), repository_name);
+    init_folder_structure.then(value =>{
+      var tarball_url = repository_information.latest_package_json.dist.tarball;
+      var tarball_name = tarball_url.split('/')[tarball_url.split('/').length - 1];
+      var tarball_storage_path = properties.getStoringFolder() + path.sep + repository_name + path.sep + 'source_code' + path.sep + 'compressed';
+      var tarball_extract_path = properties.getStoringFolder() + path.sep + repository_name + path.sep + 'source_code' + path.sep + 'raw';
+      
+      downloader.getProjectTarball(tarball_url, tarball_storage_path + path.sep + tarball_name).then(value => {
+        downloader.extractProjectTarball(tarball_storage_path + path.sep + tarball_name, tarball_extract_path + path.sep + repository_name).then(value =>{
+          resolve(repository_information._id + ': Downlad source code completed successfully');
+        })
+        .catch(err => {
+          reject('Tarball extract failed');
+        });
       })
       .catch(err => {
-        console.log('Tarball extract failed');
+        reject('Download tarball failed');
       });
     })
     .catch(err => {
-      console.log('Download tarball failed');
+      reject('Initialize structure failed: ' + err);
     });
-  })
-  .catch(err => {
-    console.log('Initialize structure failed: ' + err);
   });
-
 };
 
+function remove_source(eslint_results_obj){
 
-// p = downloader.getProjectInfo(DB_URL, DB_name, 'bootstrap');
-// Promise.all([p]).then(values => {
-//   var info = JSON.parse(values[0]);
-  
-//   getRepositorySource(info);
-//   //console.log(JSON.parse(values[0])._id);
+  return new Promise((resolve, reject) => {
+    for (var i = 0; i < eslint_results_obj.results.length; i++){
+      delete eslint_results_obj.results[i].source;
+      //delete eslint_results_obj.results[i].messages;
+    }
+    resolve(eslint_results_obj)
+  });
+};
+
+function analyze_code(project_name, platform){
+
+  return new Promise((resolve, reject) => {
+    var project_root = properties.getStoringFolder() + '\\' + project_name + '\\source_code\\raw\\' + project_name + '\\package';
+    var analysis_storing_path = properties.getStoringFolder() + '\\' + project_name + '\\analysis_results'
+
+    p = lib.get_list_of_js_files(project_root);
+    p.then(val =>{
+      jssa.analyze_all(project_root, val, platform).then(res => {
+        Array("escomplex", "nsp", "jsinspect", "eslint", "sonarjs").forEach(tool_name => {
+          if(tool_name == "eslint"){
+            remove_source(res[tool_name]).then(pure =>{
+              bfj.write(analysis_storing_path + '\\' + tool_name + ".json", pure, {}).then(() => {
+
+              })
+              .catch(error => {
+                reject(tool_name + ' results storage failed');
+              });
+            });
+          }
+          else{
+            jsonfile.writeFile(analysis_storing_path + '\\' + tool_name + ".json", res[tool_name], function (err) {
+              if(err){
+                reject(tool_name + ' results storage failed');
+              }
+            });
+          }
+        });
+        resolve(project_name + ": Analysis Successfull");
+      })
+      .catch(err => {
+        reject('jssa failed: ' + err);
+      });  
+    })
+    .catch(err => {
+      reject('Get js files failed: ' + err);
+    });
+  });
+};
+
+function run_full_analysis(DB_URL, DB_name, num_projects){
+  p = downloader.getMostStarredProjectsInfo(DB_URL, DB_name, num_projects);
+  p.then(most_starred_projects_info => {
+    for (var i = 0; i < most_starred_projects_info.rows.length; i++ ){
+      var repo = most_starred_projects_info.rows[i];
+      console.log(repo.id + ': Started analysis of repo');
+      downloader.getProjectInfo(DB_URL, DB_name, repo.id).then(repo_info => {
+        var repo_full_name = repo_info.latest_package_json.name + '-' + repo_info.latest_package_json.version
+        getRepositorySource(repo_info).then(output => {
+          console.log(output);
+          analyze_code(repo_full_name, "WINDOWS").then(analysis_output => {
+            console.log(analysis_output);  
+          })
+          .catch(err =>{
+            console.log(err);
+          });
+        })
+        .catch(err =>{
+          console.log(err);
+        });
+      })
+      .catch(err =>{
+
+      });
+    }
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+} 
+
+// Run full analysis
+run_full_analysis(DB_URL, DB_name, 2);
+
+// var project_name = 'test';
+// //var root = properties.getStoringFolder() + '\\' + project_name + '\\source_code\\raw\\' + project_name + '\\package';
+// var root = properties.getStoringFolder() + '\\test' ;
+// p = lib.get_list_of_js_files(root);
+// p.then(val =>{
+//   jssa.analyze_nsp(root, "WINDOWS").then(out =>{
+//     console.log(JSON.stringify(out, null, 2));
+    
+    
+//   })
+//   .catch(err =>{
+//     console.log(err);
+//   });
 // })
-// .catch(function(err) {
-//   console.log(err); // some coding error in handling happened
+// .catch(err =>{
+//   console.log(err);
 // });
